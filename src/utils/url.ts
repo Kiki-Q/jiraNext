67ec8fca9c2043c4bd54acca4b6ae5b4efbb445d
Kiki-Q <dingqilove@iCloud.com>
2023-10-03 00:39:11 +0800

    url

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,13 +1,17 @@
 import { useMemo } from 'react';
 import { useSearchParams } from 'react-router-dom'
+import { cleanObject } from 'utils';
 
 export const useUrlQueryParams = <K extends string>(keys: K[]) => {
     const [searchParms, setSearchParms] = useSearchParams();
     return [
         useMemo( ()=> keys.reduce((prev, key)=>{
                 return {...prev, [key]: searchParms.get(key) || ''}
-            }, {})
+            }, {} as {[key in K]: string})
         ,[searchParms]),
-        setSearchParms
-    ]
+        (params: Partial< {[key in K]:unknown} >) => {
+            const o = cleanObject({...Object.fromEntries(searchParms),...params}) as URLSearchParams;
+            return setSearchParms(o);
+        }
+    ] as const
 }
